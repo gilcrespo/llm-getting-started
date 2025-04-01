17a0aab951d859f41527c5e0db16fd0e7c2aa079
mlane <mlane@travelpassgroup.com>
2025-03-31 18:13:41 -0600

    Added rag_with_langchain_pinecone.py (todo)

---
+++ projects/rag_with_langchain_pinecone.py
@@ -0,0 +1,16 @@
+"""
+rag_with_langchain_pinecone.py
+
+A simple LangChain demo that retrieves relevant documents from a vector store (Pinecone)
+and uses them to augment responses from a language model (LLM). This demonstrates
+RAG (Retrieval Augmented Generation) using Pinecone and OpenAI's embeddings.
+
+Concepts:
+RAG, chain of thought, conversational flow, document retrieval, interactive session,
+model augmentation, semantic search, vectorization
+
+See:
+https://docs.pinecone.io/guides/get-started/overview
+"""
+
+# TODO: Implement this
